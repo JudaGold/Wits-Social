33691e3f3ed3aa2f47d23b69b87da4901da28d63
David <2355348@students.wits.ac.za>
2022-05-18 20:41:00 +0200

    Sorted the posts by time in descending order

--- SoftwareProjects/app/src/main/java/com/example/softwareproject/Main_Profile.java
+++ SoftwareProjects/app/src/main/java/com/example/softwareproject/Main_Profile.java
@@ -10,7 +10,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,24 +26,27 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Hashtable;
 import java.util.Random;
-import java.util.Vector;
 
 public class Main_Profile extends AppCompatActivity {
-    TextView usernameText, bioText;
-    EditText popup_post_body,popup_post_image;
+    TextView usernameText, bioText;// bioText will have the user's bio
+    EditText popup_post_body, popup_post_image;
     Button popup_add_post;
     ImageButton btnadd_post;
-    DatabaseReference reference;
-    ImageView user_image;
-    long maxId  = 1 ;
+    DatabaseReference reference;// this the reference of the Firebase database
+    ImageView user_image;// this will have be image on the main feed page
+    long maxId = 1;
     String username;
-    ArrayList<String> following = new ArrayList<String>();
-    Hashtable<String, Integer> username_colours = new Hashtable<String, Integer>();
+    ArrayList<String> all_usernames = new ArrayList<>();/* this will have the user's username
+                                                           and the usernames of the users, the
+                                                           user is following*/
+    Hashtable<String, Integer> username_colours = new Hashtable<>();/* this will have the unique
+                                                                       colour of each user*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,9 +57,9 @@ public class Main_Profile extends AppCompatActivity {
         bioText = (TextView) findViewById(R.id.user_bio);
         username = intent.getStringExtra("username");
         user_image = (ImageView) findViewById(R.id.user_image);
+        all_usernames.add(username);
         set_user_profile();
         getFollowing();
-        //display_posts();
         btnadd_post = (ImageButton) findViewById(R.id.btn_add_post);
         btnadd_post.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,31 +71,30 @@ public class Main_Profile extends AppCompatActivity {
         user_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent2 = new Intent(Main_Profile.this,Show_Profile_Details.class);
-                intent2.putExtra("Username",username);
+                Intent intent2 = new Intent(Main_Profile.this, Show_Profile_Details.class);
+                intent2.putExtra("Username", username);
                 startActivity(intent2);
             }
         });
 
     }
 
-    public void set_user_profile( ) {
+    public void set_user_profile() {
         reference = FirebaseDatabase.getInstance().getReference("Users");
         Query checkUser = reference.orderByChild("username").equalTo(username);
 
         checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
+                if (snapshot.exists()) {
                     usernameText.setText(username);
                     String bio = snapshot.child(username).child("bio").getValue(String.class);
                     String imageUrl = snapshot.child(username).child("mImageUrl").getValue(String.class);
                     Picasso.get().load(imageUrl).into(user_image);
 
-                    if(bio.length()>=1){
+                    if (bio.length() >= 1) {
                         bioText.setText(bio);
-                    }
-                    else{
+                    } else {
                         bioText.setHint("(Bio will be displayed here)");
                     }
                 }
@@ -106,12 +107,12 @@ public class Main_Profile extends AppCompatActivity {
         });
     }
 
-    public void add_post( ){
+    public void add_post() {
         AlertDialog.Builder dialogB = new AlertDialog.Builder(this);
         AlertDialog dialog;
-        final View popup_content = getLayoutInflater().inflate(R.layout.popup_post,null);
-        popup_post_body= (EditText) popup_content.findViewById(R.id.post_body);
-        popup_post_image =(EditText) popup_content.findViewById(R.id.post_image);
+        final View popup_content = getLayoutInflater().inflate(R.layout.popup_post, null);
+        popup_post_body = (EditText) popup_content.findViewById(R.id.post_body);
+        popup_post_image = (EditText) popup_content.findViewById(R.id.post_image);
         popup_add_post = (Button) popup_content.findViewById(R.id.btn_post);
         dialogB.setView(popup_content);
         dialog = dialogB.create();
@@ -122,75 +123,91 @@ public class Main_Profile extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                 try {
+                try {
                     Post post;
                     String body = popup_post_body.getText().toString();
                     //String image_url = popup_post_image.getText().toString()
-                     Date date = new Date();
-                     SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-                     String t = (format.format(date)).toString();
+                    Date date = new Date();
+                    SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+                    String t = (format.format(date));
 
-                     post = new Post(body,"",t);
-                     reference = FirebaseDatabase.getInstance().getReference("Posts").child(username);
+                    post = new Post(body, "", t);
+                    reference = FirebaseDatabase.getInstance().getReference("Posts").child(username);
 
-                     reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    reference.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                if(snapshot.exists()) {
-                                    maxId = (snapshot.getChildrenCount()) + 1;
-                                }
+                            if (snapshot.exists()) {
+                                maxId = (snapshot.getChildrenCount()) + 1;
+                            }
                             reference.child(String.valueOf(maxId)).setValue(post);
                             dialog.dismiss();
-                            display_posts(following, username_colours);
+                            fetchPosts(all_usernames, username_colours);
                         }
+
                         @Override
                         public void onCancelled(@NonNull DatabaseError error) {
 
                         }
                     });
-                }catch(Exception e){
+                } catch (Exception e) {
                 }
             }
         });
     }
 
-    public void display_posts(ArrayList<String> following, Hashtable<String, Integer> username_colours){
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void display_posts(ArrayList<Post> Posts, Hashtable<String, Integer> username_colours) {
         LinearLayout lp = (LinearLayout) findViewById(R.id.scroll_posts);
         lp.setOrientation(LinearLayout.VERTICAL);
         lp.setBackgroundColor(Color.LTGRAY);
         lp.removeAllViews();
-        reference = FirebaseDatabase.getInstance().getReference().child("Posts").child(username);
-        Query posts = reference.orderByChild(String.valueOf(maxId));
-        posts.addListenerForSingleValueEvent(new ValueEventListener() {
+        for (Post post : Posts) {
+            String post_body = post.getBody();
+            String post_time = post.getTime();
+            String username_post = post.getUsername();
+
+            TextView usernameView = new TextView(getApplicationContext());
+            usernameView.setTextSize(20);
+
+            if (username_post.equalsIgnoreCase(username))
+            {
+                usernameView.setText("Me:");
+                usernameView.setTextColor(Color.parseColor("#ff0099cc"));
+            }
+            else
+            {
+                usernameView.setText(username_post + ":");
+                usernameView.setTextColor(username_colours.get(username_post));
+            }
+            lp.addView(usernameView);
+
+            TextView postView = new TextView(getApplicationContext());
+            postView.setTextSize(20);
+            postView.setPadding(30, 30, 30, 30);
+            postView.setText(post_body + "\n" + post_time);
+            postView.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.post_layout));
+            lp.addView(postView);
+
+        }
+    }
+
+    public void getFollowing() {
+        reference = FirebaseDatabase.getInstance().getReference("Following").child(username);
+
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Vector<Post>post_data;
-                post_data = new Vector<Post>();
-                for(DataSnapshot data:snapshot.getChildren()){
-                    String b = data.child("body").getValue(String.class);
-                    String t = data.child("time").getValue(String.class);
-                    post_data.add(new Post(b,"",t));
+                for (DataSnapshot data : snapshot.getChildren()) {
+                    String following_username = data.getValue(String.class);
+                    all_usernames.add(following_username);
+                    Random rnd = new Random();
+                    int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                    username_colours.put(following_username, color);
                 }
-                for(int i = post_data.size()-1;i>=0;i--){
-                    String post_body = post_data.elementAt(i).getBody();
-                    String post_time = post_data.elementAt(i).getTime();
-
-                    TextView username = new TextView(getApplicationContext());
-                    username.setText("Me:");
-                    username.setTextSize(20);
-                    username.setTextColor(Color.parseColor("#ff0099cc"));
-                    lp.addView(username);
-
-                    TextView post = new TextView(getApplicationContext());
-                    post.setTextSize(20);
-                    post.setPadding(30,30,30,30);
-                    post.setText(post_body+"\n"+post_time);
-                    post.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.post_layout));
-                    lp.addView(post);
-
-                }
-
+                fetchPosts(all_usernames, username_colours);
             }
 
             @Override
@@ -198,41 +215,32 @@ public class Main_Profile extends AppCompatActivity {
 
             }
         });
+    }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void fetchPosts(ArrayList<String> following, Hashtable<String, Integer> username_colours) {
+        ArrayList<Post> Posts = new ArrayList<>();
 
         for (String usernames : following) {
             reference = FirebaseDatabase.getInstance().getReference().child("Posts").child(usernames);
             Query following_posts = reference.orderByChild(String.valueOf(maxId));
             following_posts.addListenerForSingleValueEvent(new ValueEventListener() {
-                @RequiresApi(api = Build.VERSION_CODES.O)
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    Vector<Post>post_data;
-                    post_data = new Vector<Post>();
-                    for(DataSnapshot data:snapshot.getChildren()){
+                    for (DataSnapshot data : snapshot.getChildren()) {
                         String b = data.child("body").getValue(String.class);
                         String t = data.child("time").getValue(String.class);
-                        post_data.add(new Post(b,"",t));
+                        Post post = new Post(b, "", t);
+                        post.setUsername(usernames);
+                        try {
+                            post.convertDate();
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                        Posts.add(post);
                     }
-                    for(int i = post_data.size()-1;i>=0;i--){
-                        String post_body = post_data.elementAt(i).getBody();
-                        String post_time = post_data.elementAt(i).getTime();
-
-                        TextView username = new TextView(getApplicationContext());
-                        username.setText(usernames + ":");
-                        username.setTextSize(20);
-                        username.setTextColor(username_colours.get(usernames));
-                        lp.addView(username);
-
-                        TextView post = new TextView(getApplicationContext());
-                        post.setTextSize(20);
-                        post.setPadding(30,30,30,30);
-                        post.setText(post_body+"\n"+post_time);
-                        post.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.post_layout));
-                        lp.addView(post);
-
-                    }
-
+                    Posts.sort(new DateComparator());
+                    display_posts(Posts, username_colours);
                 }
 
                 @Override
@@ -241,33 +249,6 @@ public class Main_Profile extends AppCompatActivity {
                 }
             });
         }
-    }
-
-    public void getFollowing()
-    {
-        reference = FirebaseDatabase.getInstance().getReference("Following").child(username);
-
-        reference.addListenerForSingleValueEvent(new ValueEventListener(){
-
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot data:snapshot.getChildren())
-                {
-                    String following_username = data.getValue(String.class);
-                    following.add(following_username);
-                    Random rnd = new Random();
-                    int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
-                    username_colours.put(following_username, color);
-                }
-
-                display_posts(following, username_colours);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
     }
 
 }
--- SoftwareProjects/app/src/main/java/com/example/softwareproject/Post.java
+++ SoftwareProjects/app/src/main/java/com/example/softwareproject/Post.java
@@ -1,19 +1,18 @@
 package com.example.softwareproject;
 
-import android.media.Image;
 import android.os.Build;
-
 import androidx.annotation.RequiresApi;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
+import java.util.*;
 
 @RequiresApi(api = Build.VERSION_CODES.O)
-public class Post {
+public class Post
+{
     private String username,body,post_image_url,time;
-    private Image image_url;
+    private Date date;// this will store the actual date of the post
+
     public Post(String body, String post_image_url, String time) {
         this.time= time;
         this.body = body;
@@ -51,5 +50,36 @@ public class Post {
 
     public void setTime(String time) {
         this.time = time;
+    }
+
+    // this method will convert the string "time" to a date object
+    public void convertDate() throws ParseException {
+        this.date = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").parse(this.time);
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
+
+// this class will compare the dates and times of the posts
+class DateComparator implements Comparator<Post> {
+    // override the compare() method
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public int compare(Post p1, Post p2)
+    {
+        // if the first post's date and time is later than the second post's date and time
+        if (p1.getDate().compareTo(p2.getDate()) > 0)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
     }
 }
