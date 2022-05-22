d4300df8041ef62b650fa4600a3d2e0f0adeda2c
Naazni Jagdew <1852151@studentswits.ac.za>
2022-05-22 18:42:46 +0200

    removed the set cases in user test class

--- SoftwareProjects/app/src/test/java/com/example/softwareproject/User_Test.java
+++ SoftwareProjects/app/src/test/java/com/example/softwareproject/User_Test.java
@@ -10,16 +10,7 @@ class User_Test {
         User fakeUser = new User("Elementrix08",
                 "verushannaidoo@gmail.com", "0615805094", "Elementrix",
                 "Verushan Naidoo", "Talk is cheap, show me the code", "-");
-
-        fakeUser.setBio("Talk is cheap, show me the code");
-        fakeUser.setUsername("Elementrix08");
-        fakeUser.setEmail("verushannaidoo@gmail.com");
-        fakeUser.setmImageUrl("-");
-        fakeUser.setName("Verushan Naidoo");
-        fakeUser.setPassword("Elementrix");
-        fakeUser.setPhoneNumber("0615805094");
-
-
+        
         assertEquals(fakeUser.getName(), "Verushan Naidoo");
         assertEquals(fakeUser.getUsername(), "Elementrix08");
         assertEquals(fakeUser.getBio(), "Talk is cheap, show me the code");
