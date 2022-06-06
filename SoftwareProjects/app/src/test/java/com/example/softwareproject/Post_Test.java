a8721a00a2529f3b3af1a49f30ef269610061b14
David <2355348@students.wits.ac.za>
2022-06-06 15:26:18 +0200

    Added comments

--- SoftwareProjects/app/src/test/java/com/example/softwareproject/Post_Test.java
+++ SoftwareProjects/app/src/test/java/com/example/softwareproject/Post_Test.java
@@ -12,8 +12,11 @@ class Post_Test {
 
     @Test
     public void Post_initialState_validOutput(){
+        // Creating a post
+        // Initializing the post
         Post fakePost= new Post("1","Hello Everyone","__","25-03-2000 13:55:55");
 
+        // Verifying the post's get methods outputs
         assertEquals(fakePost.getBody(),"Hello Everyone");
         assertEquals(fakePost.getPost_image_url(),"__");
         assertEquals(fakePost.getTime(),"25-03-2000 13:55:55");
@@ -22,13 +25,16 @@ class Post_Test {
 
     @Test
     public void Post_postCreation_validOutput(){
+        // Creating a post
         Post fakePost= new Post("1","Hello Everyone","__","25-03-2000 13:55:55");
 
+        // Setting the post's details
         fakePost.setUsername("David The Boy");
         fakePost.setBody("Hello its David");
         fakePost.setTime("00:00");
         fakePost.setPost_image_url("---");
 
+        // Verifying the post's get methods outputs
         assertEquals(fakePost.getBody(),"Hello its David");
         assertEquals(fakePost.getPost_image_url(),"---");
         assertEquals(fakePost.getTime(),"00:00");
