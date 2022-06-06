b997d1229c7708eee7d3ace81017be61e35dd687
Shelby947 <102577630+Shelby947@users.noreply.github.com>
2022-06-06 13:38:36 +0200

    Update Forgot_Password.java

--- SoftwareProjects/app/src/main/java/com/example/softwareproject/Forgot_Password.java
+++ SoftwareProjects/app/src/main/java/com/example/softwareproject/Forgot_Password.java
@@ -18,6 +18,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+//importshit
 public class Forgot_Password extends AppCompatActivity {
     EditText emailfield,p1,p2;
     Button btn,cbtn;
