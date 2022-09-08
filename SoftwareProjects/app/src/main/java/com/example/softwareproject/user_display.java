6c5fd24b8606f1755c70af53a24b6bbe761ad563
aharon_g <aharon.grossberg57@gmail.com>
2022-09-08 23:41:46 +0200

    Fixing ability to respond to a reply

--- SoftwareProjects/app/src/main/java/com/example/softwareproject/user_display.java
+++ SoftwareProjects/app/src/main/java/com/example/softwareproject/user_display.java
@@ -70,6 +70,7 @@ public class user_display extends AppCompatActivity {
         bioText = (TextView) findViewById(R.id.bio_text);
         user_image = findViewById(R.id.searched_user_image);
         btnfollow = new Button(getApplicationContext());
+        btnfollow.setText("follow");
         display_user_information();
 
         if(username.equalsIgnoreCase(logged_in_user)){
