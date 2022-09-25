b4826ac8c9db4df7ec3267a7da05ba8237104274
Naazni Jagdew <1852151@studentswits.ac.za>
2022-09-25 23:57:34 +0200

    created an additional test class
    also made changes to the commentary in the fcmTest class .

--- SoftwareProjects/app/src/test/java/com/example/softwareproject/FcmNotificationsSenderTest.java
+++ SoftwareProjects/app/src/test/java/com/example/softwareproject/FcmNotificationsSenderTest.java
@@ -7,9 +7,8 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.jupiter.api.Test;
 
 class FcmNotificationsSenderTest {
-
+    //test to check if we get the valid output for the FcmNotificactionSender class
     @Test
-
     public void FcmNotificationSender_initialState_validOutput(){
         FcmNotificationsSender fakeTest = new FcmNotificationsSender("Naazni Token","New fake title","Hellow there ",InstrumentationRegistry.getInstrumentation().getContext());
         assertEquals(fakeTest.getClass(),"Naazni Token");
