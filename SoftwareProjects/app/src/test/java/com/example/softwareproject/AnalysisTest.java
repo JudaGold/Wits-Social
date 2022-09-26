34b696db54e151c0b7a4cfb13bd2d4ed65848638
Naazni Jagdew <1852151@studentswits.ac.za>
2022-09-26 19:47:26 +0200

    Analysis test class function update

---
+++ SoftwareProjects/app/src/test/java/com/example/softwareproject/AnalysisTest.java
@@ -0,0 +1,26 @@
+package com.example.softwareproject;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class
+AnalysisTest {
+
+    //test to check if we get the valid output for the text body in analysis class
+    @Test
+    public void Analysis_initialState_validOutput(){
+
+        Analysis fakebody= new Analysis("this is a fake body text");
+
+        assertEquals(fakebody.post_body ,"this is a fake body text");
+    }
+
+    public void valid_url_initialState_validOutput(){
+        String fakeValidUrl = ("http://example.com");
+        boolean validUrl = Analysis.valid_URL(fakeValidUrl);
+
+    }
+    
+
+}
